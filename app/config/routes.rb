c3e6ee48621fb8f573688e9a41c38ee323db4d5a
Spundun Bhatt <spundun@gmail.com>
2015-04-26 09:47:50 -0700

    Configured routes according to specs

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :shorts
+  resources :shorts, only: [:show, :new, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root to: redirect('shorts/new')
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  get '/:id' => 'shorts#point'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
