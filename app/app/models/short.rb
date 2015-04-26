d214fba86b5f5feb89352fb61044b40a5de23701
Spundun Bhatt <spundun@gmail.com>
2015-04-26 12:09:22 -0700

    Implement URL validation

--- app/app/models/short.rb
+++ app/app/models/short.rb
@@ -1,3 +1,13 @@
+require 'uri'
+
 class Short < ActiveRecord::Base
-	validates :url, presence: true
+    validates :url, presence: true
+    validates_each :url do |record, attrib, value|
+      begin
+        uri = URI.parse(value)
+        raise "Incorrect protocol" unless uri.kind_of?(URI::HTTP)
+      rescue
+        record.errors.add(attrib, "Invalid URL, please post a valid complete URL (including the http/https part)")
+      end
+    end
 end
--- app/spec/views/shorts/show.html.erb_spec.rb
+++ app/spec/views/shorts/show.html.erb_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe "shorts/show", type: :view do
   before(:each) do
     @short = assign(:short, Short.create!(
-      :url => "MyText"
+      :url => "http://google.com"
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/http:\/\/google\.com/)
   end
 end
