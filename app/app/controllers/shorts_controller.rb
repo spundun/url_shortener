02bec2ff09c1b36b71fcc52e4337ac692519feb6
Spundun Bhatt <spundun@gmail.com>
2015-04-26 09:55:28 -0700

    Taking out unnecessary code and view specs

--- app/app/controllers/shorts_controller.rb
+++ app/app/controllers/shorts_controller.rb
@@ -1,11 +1,5 @@
 class ShortsController < ApplicationController
-  before_action :set_short, only: [:show, :edit, :update, :destroy]
-
-  # GET /shorts
-  # GET /shorts.json
-  def index
-    @shorts = Short.all
-  end
+  before_action :set_short, only: [:show, :destroy]
 
   # GET /shorts/1
   # GET /shorts/1.json
@@ -15,10 +9,6 @@ class ShortsController < ApplicationController
   # GET /shorts/new
   def new
     @short = Short.new
-  end
-
-  # GET /shorts/1/edit
-  def edit
   end
 
   # POST /shorts
@@ -34,30 +24,6 @@ class ShortsController < ApplicationController
         format.html { render :new }
         format.json { render json: @short.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /shorts/1
-  # PATCH/PUT /shorts/1.json
-  def update
-    respond_to do |format|
-      if @short.update(short_params)
-        format.html { redirect_to @short, notice: 'Short was successfully updated.' }
-        format.json { render :show, status: :ok, location: @short }
-      else
-        format.html { render :edit }
-        format.json { render json: @short.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /shorts/1
-  # DELETE /shorts/1.json
-  def destroy
-    @short.destroy
-    respond_to do |format|
-      format.html { redirect_to shorts_url, notice: 'Short was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- app/app/views/shorts/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@shorts) do |short|
-  json.extract! short, :id, :url
-  json.url short_url(short, format: :json)
-end
