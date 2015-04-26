ba69c0d9772b24edad2c8c789854c015ab5c6dcd
Spundun Bhatt <spundun@gmail.com>
2015-04-26 10:23:37 -0700

    Wrote routing specs

--- app/spec/routing/shorts_routing_spec.rb
+++ app/spec/routing/shorts_routing_spec.rb
@@ -3,10 +3,6 @@ require "rails_helper"
 RSpec.describe ShortsController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/shorts").to route_to("shorts#index")
-    end
-
     it "routes to #new" do
       expect(:get => "/shorts/new").to route_to("shorts#new")
     end
@@ -15,20 +11,30 @@ RSpec.describe ShortsController, type: :routing do
       expect(:get => "/shorts/1").to route_to("shorts#show", :id => "1")
     end
 
-    it "routes to #edit" do
-      expect(:get => "/shorts/1/edit").to route_to("shorts#edit", :id => "1")
-    end
-
     it "routes to #create" do
       expect(:post => "/shorts").to route_to("shorts#create")
     end
 
-    it "routes to #update" do
-      expect(:put => "/shorts/1").to route_to("shorts#update", :id => "1")
+    #Route to the point method
+    it "should route /:id to #point(:id) (for actual short url dereferencing)" do
+      expect(get: "/1").to route_to("shorts#point", id: "1")
     end
 
-    it "routes to #destroy" do
-      expect(:delete => "/shorts/1").to route_to("shorts#destroy", :id => "1")
+    it "should treat route to #index as an invalid route to point" do
+      expect(:get => "/shorts").to route_to("shorts#point", id: "shorts")
+    end
+
+    #Blocked resource routes
+    it "blocks routes to #edit" do
+      expect(:get => "/shorts/1/edit").not_to be_routable
+    end
+
+    it "blocks routes to #update" do
+      expect(:put => "/shorts/1").not_to be_routable
+    end
+
+    it "blocks routes to #destroy" do
+      expect(:delete => "/shorts/1").not_to be_routable
     end
 
   end
